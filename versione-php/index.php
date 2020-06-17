60b036994c6bfd3d23ea187637473f9c801c2d5d
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-06-17 16:57:01 +0200

    php collegamento array con html

--- versione-php/index.php
+++ versione-php/index.php
@@ -1,8 +1,7 @@
 <?php include('../database/dischi.php');
-var_dump($dischi);
-
+// var_dump($dischi);
 ?>
-
+<?php var_dump($dischi['poster']); ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
@@ -10,6 +9,14 @@ var_dump($dischi);
         <title></title>
     </head>
     <body>
-
+        <?php foreach ($dischi as $disco) { ?>
+        <div class="cd">
+                    <img src="<?php echo $dischi['poster']; ?>" alt="">
+                    <h3><?php  ?></h3>
+                    <span class="author"><?php  ?></span>
+                    <span class="year"><?php  ?></span>
+                    <span class="genre"><?php  ?></span>
+        </div>
+    <?php } ?>
     </body>
 </html>
