e12419beff5c25e30bac63b402c22470cbbab20f
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-06-17 17:40:14 +0200

    parte versione php dischi finita

--- versione-php/index.php
+++ versione-php/index.php
@@ -4,16 +4,19 @@
     <head>
         <meta charset="utf-8">
         <title>Dischi php</title>
+        <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        <?php foreach ($dischi as $disco) { ?>
-        <div class="cd">
+        <div class="cds-container container">
+            <?php foreach ($dischi as $disco) { ?>
+                <div class="cd">
                     <img src="<?php echo $disco['poster']; ?>" alt="">
                     <h3><?php echo $disco['title']; ?></h3>
                     <span class="author"><?php echo $disco['author']; ?></span>
                     <span class="year"><?php echo $disco['year']; ?></span>
                     <span class="genre"><?php echo $disco['genre']; ?></span>
+                </div>
+            <?php } ?>
         </div>
-    <?php } ?>
     </body>
 </html>
