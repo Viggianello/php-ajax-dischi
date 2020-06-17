8b37e968b053723e3f4efca7aaf6f97a23ef59e9
Antonello Viggiano <antonelloviggiano1990@hotmail.it>
2020-06-17 16:37:17 +0200

    dischi error fixed

--- versione-php/index.php
+++ versione-php/index.php
@@ -1,1 +1,15 @@
-<?php require 'database/dischi.php'; ?>
+<?php include('../database/dischi.php');
+var_dump($dischi);
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+
+    </body>
+</html>
